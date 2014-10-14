847de490f62a12752ea09f2f77e3ae23915f16fe
amtliz <lizrfp@gmail.com>
2014-10-14 10:21:41 -0600

    Update autenticar.php

--- paginas/autenticar.php
+++ paginas/autenticar.php
@@ -1,14 +1,14 @@
 <?php        
 if (isset($_POST['login'])) {
 extract($_POST);
-$_SESSION['autenticado']='si'; 
 
 $objeto = new CasaMunoz;
 $consulta = $objeto->consultar_usuario ($username, $password);
 
 if ($consulta->rowCount()>0){
   $resUser = $consulta->fetch(PDO::FETCH_OBJ);
-	  if($resUser->estado_usuario=='Activo'){   
+	  if($resUser->estado_usuario=='Activo'){  
+	  $_SESSION['autenticado']='si'; 
 	    if( $resUser->ROL_cod_rol == 1)     {
 	    $_SESSION['tipo']=1;
 	    } elseif($resUser->ROL_cod_rol == 2) {
