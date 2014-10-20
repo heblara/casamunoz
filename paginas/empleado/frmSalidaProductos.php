811826d00894d2aebe5ca913b7b199ba0edd7a7c
amtliz <lizrfp@gmail.com>
2014-10-20 14:22:11 -0600

    Create frmSalidaProductos.php

---
+++ paginas/empleado/frmSalidaProductos.php
@@ -0,0 +1,158 @@
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css">
+<script>
+  $(function() {
+    $( ".datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      dateFormat:'yy-mm-dd'
+    });
+  });
+</script>
+<script src="js/mask.js"></script>
+<script language="JavaScript">
+var objeto = false;
+function procesaResultado() {
+// Si aun esta revisando los datos...
+if (objeto.readyState == 1) {
+//  document.getElementById('resultado').innerHTML = "Cargando datos con ajax...";
+  document.getElementById('resultado').innerHTML = "<td colspan='6'><img src='paginas/5-0.gif' title='Cargando datos' width='32' />";
+}
+// Si el estado es 4 significa que ya termino
+if (objeto.readyState == 4) {
+  // objeto.responseText trae el Resultado que metemos al DIV de arriba
+  document.getElementById('resultado').innerHTML = objeto.responseText;
+}
+}
+function crearObjeto() {
+  // --- Crear el Objeto dependiendo los diferentes Navegadores y versiones ---
+  try { objeto = new ActiveXObject("Msxml2.XMLHTTP");  }
+  catch (e) {
+  try { objeto = new ActiveXObject("Microsoft.XMLHTTP"); }
+  catch (E) {
+  objeto = false; }
+  }
+  // --- Si no se pudo crear... intentar este ultimo metodo ---
+  if (!objeto && typeof XMLHttpRequest!='undefined') {
+    objeto = new XMLHttpRequest();
+  }
+}
+
+// ------------------------------
+
+function leerDatos(valor,sel,opcion) {
+  crearObjeto();
+  if (objeto.readyState != 0) {
+    alert('Error al crear el objeto XML. El Navegador no soporta AJAX');
+  } else {
+    // Preparar donde va a recibir el Resultado
+    objeto.onreadystatechange = procesaResultado;
+var ca=/^[ ]{1}/;
+var com=ca.test(valor);
+	if((!com=="") || (valor=="")){document.getElementById("resultado").innerHTML="";}else{
+	// Enviar la consulta
+	    objeto.open("GET", "paginas/busqueda.php?opc="+sel+"&usuario=" + valor + "&inscrito="+opcion, true);
+	    objeto.send(null);
+	}
+    
+  }
+}
+function leerDatosPag(valor,sel,pag,opcion) {
+  crearObjeto();
+  if (objeto.readyState != 0) {
+    alert('Error al crear el objeto XML. El Navegador no soporta AJAX');
+  } else {
+    // Preparar donde va a recibir el Resultado
+    objeto.onreadystatechange = procesaResultado;
+var ca=/^[ ]{1}/;
+var com=ca.test(valor);
+	if((!com=="") || (valor=="")){document.getElementById("resultado").innerHTML="";}else{
+	// Enviar la consulta
+	    objeto.open("GET", "paginas/busqueda.php?opc="+sel+"&usuario=" + valor + "&pagina="+pag+ "&inscrito="+opcion, true);
+	    objeto.send(null);
+	}
+    
+  }
+}
+// ------------------------------
+</script>
+<script type='text/javascript' language='Javascript'>
+function seleccionar(){
+	/*document.getElementById("cargarCaja").style="display:none;";
+	document.getElementById("fecha").style.diplay="none";*/
+	var tipo=document.hongkiat.lstTipo.options[document.hongkiat.lstTipo.selectedIndex].value;
+	//document.getElementById("txtFecha").style="display:none;";
+	document.getElementById("dui").style.display="none";
+	if(tipo==1){
+		document.getElementById("cargarCaja").style.display="none";
+		document.getElementById("dui").style.display="";
+	}else if(tipo==2){
+		document.getElementById("cargarCaja").innerHTML="<input type='text' name='txtTarjeta' onKeyPress='leerDatos(document.hongkiat.txtTarjeta.value,document.hongkiat.lstTipo.value,document.hongkiat.inscrito.value);' class='id txtinput' id='txtTarjeta' placeholder='Digite el numero de Tarjeta' autocomplete='off' tabindex='1' style='width:25%;'>";
+	}else if(tipo==3){
+		document.getElementById("cargarCaja").innerHTML="<input type='text' name='txtNombre' onKeyPress='leerDatos(document.hongkiat.txtNombre.value,document.hongkiat.lstTipo.value,document.hongkiat.inscrito.value);' class='id txtinput' id='txtNombre' placeholder='Digite el nombre a buscar' autocomplete='off' tabindex='1' style='width:25%;'>";
+	}else{
+		document.getElementById("cargarCaja").innerHTML="";
+	}
+}
+</script>
+<br />
+<br />
+<script>
+  $(function() {
+    $( ".datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      dateFormat:'yy-mm-dd'
+    });
+  });
+</script>
+<h2>SALIDA DE PRODUCTOS</h2>
+<h3>Sucursal: La Joya - Fecha actual: <?php echo date('d-m-Y H:i a'); ?></h3>
+<br /><br />
+<form name="hongkiat" id="hongkiat-form">
+<div id="wrapping" class="clearfix">
+  <section id="aligned" style='text-align:center;'>
+        <label>Fecha de reporte a generar:</label>
+        <input type="text" name="txtFecReporte" id="txtFecReporte" placeholder="Fecha" autocomplete="off" tabindex="1" class="txtinput calendar datepicker">
+	<div id="cargarCaja" style="clear:both;"></div>
+    <div id="resultado" style='background:black;font-size:15pt;'>
+    	<table width='100%'>
+    		<tr style="background:white;">
+    			<th>Codigo</th>
+    			<th>Cantidad ingresada</th>
+    			<th>Producto</th>
+    			<th>Fecha ultimo ingreso</th>
+    			<th>Fecha de vencimiento</th>
+          <th>Existencias</th>
+    		</tr>
+    		<tr style="background:silver;">
+    			<td>0101</td>
+    			<td>15</td>
+    			<td>Acetona</td>
+    			<td>24-08-2014</td>
+    			<td>29-11-2014</td>
+          <td>23</td>
+    		</tr>
+        <tr style="background:white;">
+          <td>0201</td>
+          <td>10</td>
+          <td>Agua oxigenada</td>
+          <td>24-08-2014</td>
+          <td>30-01-2015</td>
+          <td>12</td>
+        </tr>
+        <tr style="background:silver;">
+          <td>0301</td>
+          <td>8</td>
+          <td>Ablandador</td>
+          <td>22-07-2014</td>
+          <td>15-07-2015</td>
+          <td>17</td>
+        </tr>
+    	</table>
+    </div>
+    <img src="images/print.png" width="64px" />
+  </section>
+</div>
+</form>
