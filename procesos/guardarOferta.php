5dd4c9f2a9a76772926bd76212e9eb18a6973cb9
cindy28 <cindymarivera@gmail.com>
2014-11-11 13:52:44 -0600

    Create guardarOferta.php

---
+++ procesos/guardarOferta.php
@@ -0,0 +1,25 @@
+<?php
+include("header.php");
+sleep(1);
+session_start();
+$respuesta = new stdClass();
+extract($_POST);
+//print_r($_POST);
+$ofer=new CasaMunoz;
+$oferta=array($txtNombre,$message, $txtFechaRegistro, $txtFecLimite, $txtDescuento, $lstServicio);
+//print_r($empleado);
+if($ofer->registrar_oferta($oferta)){
+	$consUltimaOfer=$ofer->consultar_ultima_oferta();
+	$resUltimaOfer=$consUltimaOfer->fetch(PDO::FETCH_OBJ);
+	$sucursal=$resUltimaOfer->last_id;
+	
+	for($i=0;$i<count($seleccion);$i++){ //Este for recorre el arreglo materia, que va desde 0 (cero) hasta el total de materias que se tengan seleccionadas
+	$sucursal=array($seleccion[$i],$sucursal);
+	$guardarsucursaloferta=$ofer->guardar_oferta_sucursal($sucursal);
+	}
+	$respuesta->mensaje = 1;
+}else{
+	$respuesta->mensaje = 2;
+}
+echo json_encode($respuesta);
+?>
