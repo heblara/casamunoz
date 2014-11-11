9b4421b38eaa1c9129e085d02e763acdf3673ed6
amtliz <lizrfp@gmail.com>
2014-11-11 15:35:49 -0600

    Create frmRegistroSucursal.php

---
+++ paginas/admin/frmRegistroSucursal.php
@@ -0,0 +1,95 @@
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+<link rel="stylesheet" href="/resources/demos/style.css">
+<script type="text/javascript" src="js/jquery.blockUI.js"></script>
+<script type="text/javascript">
+  $(function() {
+    $('.datepicker').datepicker({
+      dateFormat: 'yy-mm-dd', 
+      changeMonth: true, 
+      changeYear: true, 
+      yearRange: '-40:+0'
+      });
+    });
+
+    $(function() {
+  //Se pone para que en todos los llamados ajax se bloquee la pantalla mostrando el mensaje Procesando...
+  $.blockUI.defaults.message = 'Procesando información, por favor espere... <br /><img src=\'img/load.gif\' /><br />';
+  $(document).ajaxStart($.blockUI).ajaxStop($.unblockUI);
+});
+function enviarDatos(){
+  var formulario = $("#hongkiat-form").serializeArray();
+    $.ajax({
+      type: "POST",
+      dataType: 'json',
+        url: "procesos/guardarSucursal.php",
+        data: formulario,
+    }).done(function(respuesta){
+        if(respuesta.mensaje==1){
+          alert("Registro realizado con exito");
+        }else if(respuesta.mensaje==2){
+          alert("No fue posible registrar el servicio");
+        }
+    });
+}
+$(document).ready(function(){         
+  $("#submitbtn").click(function(){
+      enviarDatos();
+      return false;
+  });
+});
+</script>
+<form name="hongkiat" id="hongkiat-form" method="post" action="#" onsubmit="return false;">
+    <div id="wrapping" class="clearfix">
+        <section id="aligned">
+        <h2>REGISTRO DE SUCURSALES</h2>
+        <label>Nombre de Sucursal:</label>
+        <input type="text" name="txtNombre" id="txtNombre" placeholder="Nombre de Sucursal" autocomplete="off" tabindex="1" class="txtinput offer">
+        <br />
+        <label>Direccion:</label>
+       <textarea name="txtDireccion" id="txtDireccion" style="width:40%;" placeholder="Direccion" tabindex="5" class="txtblock"></textarea>
+        <label>Telefono:</label>
+        <input type="text" name="txtTelefono" id="txtTelefono" placeholder="Telefono" autocomplete="off" tabindex="1" class="txtinput offer" >  
+        <br/>
+        <label>Correo:</label>
+        <input type="text" name="txtCorreo" id="txtCorreo" placeholder="Correo" autocomplete="off" tabindex="1" class="txtinput offer">
+        <br />
+
+		
+		
+		<label>Departamento: </label>
+<script type="text/javascript" src="funciones/select_dependientes.js"></script>>
+    <?php 
+      $objeto=new CasaMunoz;
+      $consultarDepartamentos=$objeto->consultar_departamentos();
+      //$consulta=mysql_query("SELECT cod_dpto, nom_dpto FROM DEPARTAMENTO");
+      // Voy imprimiendo el primer select compuesto por los paises
+      //echo $consultarDepartamentos->rowCount();
+      echo "<select name='paises' id='paises' onChange='cargaContenido(this.id)' class='selmenu'>";
+      echo "<option value='0'>Elige</option>";
+
+      while($registro=$consultarDepartamentos->fetch(PDO::FETCH_OBJ))
+      {
+        echo "<option value='".$registro->cod_dpto."'>".$registro->nom_dpto."</option>";
+      }
+      echo "</select>";
+   ?>
+        <label>Municipio: </label>
+        <span id="demoDer">
+        <select disabled="disabled" name="estados" id="estados" class='selmenu'>
+          <option value="0">Selecciona opci&oacute;n...</option>
+      </select>
+		
+     
+        <br />
+        </section>
+        <section id="aside" class="clearfix">
+        </section>
+    </div>
+    <section id="buttons">
+        <!--<input type="reset" name="reset" id="resetbtn" class="resetbtn" value="Reset">-->
+        <input type="submit" name="submit" id="submitbtn" class="submitbtn" tabindex="7" value="Guardar">
+        <br style="clear:both;">
+    </section>
+</form>
