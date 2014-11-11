34268e99805ee2464f4fed061fd48418a3cfa98b
amtliz <lizrfp@gmail.com>
2014-11-11 15:35:09 -0600

    Create frmRegistroCargo.php

---
+++ paginas/admin/frmRegistroCargo.php
@@ -0,0 +1,60 @@
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+<link rel="stylesheet" href="/resources/demos/style.css">
+<script type="text/javascript" src="js/jquery.blockUI.js"></script>
+<script type="text/javascript">
+  $(function() {
+    $('.datepicker').datepicker({
+      dateFormat: 'yy-mm-dd', 
+      changeMonth: true, 
+      changeYear: true, 
+      yearRange: '-40:+0'
+      });
+    });
+
+    $(function() {
+  //Se pone para que en todos los llamados ajax se bloquee la pantalla mostrando el mensaje Procesando...
+  $.blockUI.defaults.message = 'Procesando información, por favor espere... <br /><img src=\'img/load.gif\' /><br />';
+  $(document).ajaxStart($.blockUI).ajaxStop($.unblockUI);
+});
+function enviarDatos(){
+  var formulario = $("#hongkiat-form").serializeArray();
+    $.ajax({
+      type: "POST",
+      dataType: 'json',
+        url: "procesos/guardarCargo.php",
+        data: formulario,
+    }).done(function(respuesta){
+        if(respuesta.mensaje==1){
+          alert("Registro realizado con exito");
+        }else if(respuesta.mensaje==2){
+          alert("No fue posible registrar el servicio");
+        }
+    });
+}
+$(document).ready(function(){         
+  $("#submitbtn").click(function(){
+      enviarDatos();
+      return false;
+  });
+});
+</script>
+<form name="hongkiat" id="hongkiat-form" method="post" action="#" onsubmit="return false;">
+    <div id="wrapping" class="clearfix">
+        <section id="aligned">
+        <h2>REGISTRO DE CARGO</h2>
+        <label>Nombre Cargo:</label>
+        <input type="text" name="txtNombreCargo" id="txtNombreCargo" placeholder="Nombre Cargo" tabindex="1" class="txtinput">
+		<textarea name="txtDesc" id="txtDesc" placeholder="Descripci&oacute;n..." tabindex="11" class="txtblock"></textarea>
+		
+		</form>
+        </section>
+        <section id="aside" class="clearfix">
+        </section>
+    </div>
+    <section id="buttons">
+        <input type="submit" name="submit" id="submitbtn" class="submitbtn" tabindex="7" value="Guardar">
+        <br style="clear:both;">
+    </section>
+</form>
