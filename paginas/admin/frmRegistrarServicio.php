69e8f068e7798257c1a0107ebdaee9825bb60705
ana03cons <aconstanza03@gmail.com>
2014-11-12 01:31:59 -0600

    Update frmRegistrarServicio.php

--- paginas/admin/frmRegistrarServicio.php
+++ paginas/admin/frmRegistrarServicio.php
@@ -1,6 +1,4 @@
-
-      
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 <script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
 <link rel="stylesheet" href="/resources/demos/style.css">
@@ -25,7 +23,7 @@ function enviarDatos(){
     $.ajax({
       type: "POST",
       dataType: 'json',
-        url: "procesos/actualizarservicio.php",
+        url: "procesos/guardarservicio.php",
         data: formulario,
     }).done(function(respuesta){
         if(respuesta.mensaje==1){
@@ -45,29 +43,28 @@ $(document).ready(function(){
 <form name="hongkiat" id="hongkiat-form" method="post" action="#" onsubmit="return false;">
     <div id="wrapping" class="clearfix">
         <section id="aligned">
-        <h2>ACTUALIZAR SERVICIOS</h2>
-        <?php
-      $objeto=new CasaMunoz;
-      $id=base64_decode($_GET["id"]);
-      $consultarserv=$objeto->mostrar_servicio($id);
-      $resServicio=$consultarserv->fetch(PDO::FETCH_OBJ);
-?>  
-         <label>Codigo:</label>
-        <input type="hidden" name="txtcodigo" id="txtcodigo" placeholder="codigo  de servicio" autocomplete="off" tabindex="1" class="txtinput offer" value= "<?php echo $resServicio->cod_servicio ?>" >
-        <br />
+        <h2>REGISTRO DE SERVICIOS</h2>
+       
+
+
 
         <label>Nombre del servicio:</label>
-        <input type="text" name="txtNombre" id="txtNombre" placeholder="Nombre de servicio" autocomplete="off" tabindex="1" class="txtinput offer" value= "<?php echo $resServicio->nom_servicio ?>"  >
+        <input type="text" name="txtNombre" id="txtNombre" placeholder="Nombre de servicio" autocomplete="off" tabindex="1" class="txtinput offer">
         <br />
         <label>Descripcion:</label>
-       <textarea name="message" id="message" style="width:40%;" placeholder="Descripcion..." tabindex="5" value= "<?php echo $resServicio->desc_servicio ?>"   class="txtblock"></textarea>
-        
+       <textarea name="message" id="message" style="width:40%;" placeholder="Descripcion..." tabindex="5" class="txtblock"></textarea>
+        <label>Fecha registro de servicio:</label>
+        <input type="text" name="txtFechaRegistro" id="txtFechaRegistro" placeholder="Fecha de registro de producto" autocomplete="off" tabindex="1" class="txtinput calendar" readonly="readonly" value="<?php echo date('Y-m-d H:i:s') ?>">  
+        <br/>
         <label>precio de servicio:</label>
-        <input type="text" name="txtprecio" id="txtprecio" placeholder="precio de servicio" autocomplete="off" tabindex="1" value= "<?php echo $resServicio->cod_costo  ?>" class="txtinput money">
+        <input type="text" name="txtprecio" id="txtprecio" placeholder="precio de servicio" autocomplete="off" tabindex="1" class="txtinput money">
         <br />
 
         <label>Duraci&oacute;n (En minutos):</label>
-        <input type="time" name="txtDuracion" id="txtDuracion" placeholder="Tiempo que dura el servicio (en minutos)" property="readOnly" autocomplete="off" tabindex="1" class="txtinput time"  value= "<?php echo $resServicio->duracion_servicio ?>" >
+        <input type="time" name="txtDuracion" id="txtDuracion" placeholder="Tiempo que dura el servicio (en minutos)" property="readOnly" autocomplete="off" tabindex="1" class="txtinput time">
+        
+        <fieldset>
+        <br><label>Sucursal: </label>
         
      <?php 
       $objeto=new CasaMunoz;
@@ -78,8 +75,6 @@ $(document).ready(function(){
       }
    ?>
 
-
-
 </fieldset>
         <br />
         </section>
@@ -88,7 +83,7 @@ $(document).ready(function(){
     </div>
     <section id="buttons">
         <!--<input type="reset" name="reset" id="resetbtn" class="resetbtn" value="Reset">-->
-        <input type="submit" name="submit" id="submitbtn" class="submitbtn" tabindex="7" value="Actualizar Servicio">
+        <input type="submit" name="submit" id="submitbtn" class="submitbtn" tabindex="7" value="Guardar">
         <br style="clear:both;">
     </section>
 </form>
