c192f1c434be3da5be7360e3dddf62ae4217ac94
ana03cons <aconstanza03@gmail.com>
2014-11-12 13:16:48 -0600

    Update actualizarservicio.php

--- procesos/actualizarservicio.php
+++ procesos/actualizarservicio.php
@@ -2,18 +2,19 @@
 include("header.php");
 sleep(1);
 session_start();
-
-//echo "Sucursal: ".$_SESSION['sucursal'];
 $respuesta = new stdClass();
-extract($_POST)
-$ObjServicio=new CasaMunoz;
-$servicio=array($txtcodigo,$txtNombre,$message,$txtFechaRegistro,$txtDuracion,"1",$costo,$_SESSION['usuario']);
-
-print_r($servicio);
-if($ObjServicio->actualizar_servicio($servicio)){
-		$respuesta->mensaje = 1;
-	}else{
-		$respuesta->mensaje = 2;
-	}
+//print_r($_POST);
+//echo "<br />";
+extract($_POST); //Extrae todos los elementos del array POST
+$ObjEmpleado=new CasaMunoz;
+$servicio=array($txtcodigo,$txtNombre,$message,$txtDuracion,$txtprecio);
+//print_r($empleado);
+if($ObjEmpleado->actualizar_servicio($servicio)){
+	$respuesta->mensaje = 1;
+}else{
+	$respuesta->mensaje = 2;
+}
 echo json_encode($respuesta);
+?>on_encode($respuesta);
 ?>
+	
