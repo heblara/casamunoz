a2142c8d792e540a3d0a5feac9be5545a4d115bb
ana03cons <aconstanza03@gmail.com>
2014-11-16 00:56:32 -0600

    Update busquedaReserva.php

--- paginas/busquedaReserva.php
+++ paginas/busquedaReserva.php
@@ -11,12 +11,10 @@ if(isset($_GET['valor'])){
     echo "<tr>
     <td>No.</td>
     <td>Nombre de cliente</td>
-	<td>Apellido de cliente</td>
-	   <td>Nombre de empleado</td>
-	<td>Apellido de empleado</td>
-        <td>Nombre servicio</td>
-			<td>Fecha de reserva</td>
-			<td>Estado</td>
+    <td>Nombre de empleado</td>
+       <td>Nombre servicio</td>
+      <td>Fecha de reserva</td>
+      <td>Estado</td>
             
     </tr>";
     $i=0; 
@@ -25,13 +23,11 @@ if(isset($_GET['valor'])){
       echo "<tr>
       <td>".$i."</td>
      
-        <td>".$resReserva->primer_nom."</td>
-        <td>".$resReserva->primer_ape."</td>
-		 <td>".$resReserva->primer_nom."</td>
-        <td>".$resReserva->primer_ape."</td>
-        <td>".$resReserva->nom_servicio."</td>    
+        <td>".$resReserva->NombreCompletoCliente."</td>
+        <td>".$resReserva->NombreCompletoEmpleado."</td>
+      <td>".$resReserva->nom_servicio."</td>    
     <td>".$resReserva->fec_estado_rsv."</td> 
-	 <td>".$resReserva->estado_rsv."</td>
+   <td>".$resReserva->estado_rsv."</td>
       <td><a href='?mod=actualizarreserva&id=".base64_encode($resReserva->cod_rsv)."'><img src='img/edit.png' /></a></td>
       </tr>";
     }
@@ -41,8 +37,3 @@ if(isset($_GET['valor'])){
   }
 }
 ?>
-
-  
-
-
-  
