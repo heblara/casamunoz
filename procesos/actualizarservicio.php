9107d59539583c623df37bf2076fd27ab42f809d
amtliz <lizrfp@gmail.com>
2014-11-18 19:07:26 -0600

    Update actualizarservicio.php

--- procesos/actualizarservicio.php
+++ procesos/actualizarservicio.php
@@ -10,6 +10,10 @@ $ObjEmpleado=new CasaMunoz;
 $servicio=array($txtcodigo,$txtNombre,$message,$txtDuracion,$txtprecio);
 //print_r($empleado);
 if($ObjEmpleado->actualizar_servicio($servicio)){
+	for($i=0;$i<count($seleccion);$i++){ //Este for recorre el arreglo materia, que va desde 0 (cero) hasta el total de materias que se tengan seleccionadas
+			$sucursal=array($seleccion[$i],$txtcodigo);
+		    $guardarsucursalservicio=$ObjEmpleado->guardar_sucursal_servicio($sucursal);
+		}
 	$respuesta->mensaje = 1;
 }else{
 	$respuesta->mensaje = 2;
