6cbcd9a49ac71095be15d5f0d003ba1986aafdc4
keestrada <karlaestrada77@hotmail.com>
2014-11-19 19:39:05 -0600

    Update frmPedidos.php

--- paginas/empleado/frmPedidos.php
+++ paginas/empleado/frmPedidos.php
@@ -1,135 +1,186 @@
-<script type="text/javascript" src="js/calendar/scripts/epoch_classes.js"></script>
-<link rel="stylesheet" type="text/css" href="js/calendar/epoch_styles.css" />
-<script language="JavaScript">
-var objeto = false;
-function procesaResultado() {
-// Si aun esta revisando los datos...
-if (objeto.readyState == 1) {
-//  document.getElementById('resultado').innerHTML = "Cargando datos con ajax...";
-  document.getElementById('resultado').innerHTML = "<td colspan='6'><img src='paginas/5-0.gif' title='Cargando datos' width='32' />";
-}
-// Si el estado es 4 significa que ya termino
-if (objeto.readyState == 4) {
-  // objeto.responseText trae el Resultado que metemos al DIV de arriba
-  document.getElementById('resultado').innerHTML = objeto.responseText;
-}
-}
-function crearObjeto() {
-  // --- Crear el Objeto dependiendo los diferentes Navegadores y versiones ---
-  try { objeto = new ActiveXObject("Msxml2.XMLHTTP");  }
-  catch (e) {
-  try { objeto = new ActiveXObject("Microsoft.XMLHTTP"); }
-  catch (E) {
-  objeto = false; }
-  }
-  // --- Si no se pudo crear... intentar este ultimo metodo ---
-  if (!objeto && typeof XMLHttpRequest!='undefined') {
-    objeto = new XMLHttpRequest();
-  }
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.1/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.1/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script type="text/javascript" src="js/jquery.blockUI.js"></script>  
+<script type="text/javascript">
+
+	function validar(){
+    nombre=document.getElementById('txtNombre').value;
+    descripcion=document.getElementById('message').value;
+    fechalimite=document.getElementById('txtFecLimite').value;
+	descuento=document.getElementById('txtDescuento').value;
+	servicio=document.getElementById('lstServicio').value;
+    msg="";
+    error=false;
+	var ok = 0;
+    var ckbox = document.getElementsByName('seleccion[]');
+        for (var i=0; i < ckbox.length; i++){
+            if(ckbox[i].checked == true){
+            ok = 1;
+            }
+        }
+	if(nombre=="" || nombre==null){
+        msg+='* Nombre';
+        alert("Ingrese el nombre de la oferta");
+        //#hongkiat-form input.name
+        document.getElementsByName('txtNombre').style="border-color:#F80303;";
+        document.getElementById('txtNombre').focus();
+        error=true;
+    }else if(descripcion=="" || descripcion==null){
+        msg+='* Descripcion';
+        alert("Ingrese la descripcion");
+        //#hongkiat-form input.name
+        document.getElementsByName('message').style="border-color:#F80303;";
+        document.getElementById('message').focus();
+        error=true;
+    }else if(fechalimite=="" || fechalimite==null){
+        msg+='* Fecha';
+        alert("Ingrese la fecha limite");
+        //#hongkiat-form input.name
+        document.getElementsByName('txtFecLimite').style="border-color:#F80303;";
+        document.getElementById('txtFecLimite').focus();
+        error=true;
+	}else if(descuento=="" || descuento==null){
+        msg+='* Descuento';
+        alert("Ingrese el descuento");
+        //#hongkiat-form input.name
+        document.getElementsByName('txtDescuento').style="border-color:#F80303;";
+        document.getElementById('txtDescuento').focus();
+        error=true;
+    }else if(servicio==0){
+        msg+='* Servicio';
+        alert("Seleccione un servicio");
+        //#hongkiat-form input.name
+        document.getElementsByName('lstServicio').style="border-color:#F80303;";
+        error=true;
+	}else if(ok == 0){
+        alert('Indique una sucursal');
+        return false;
+    }
+	if(error==true){
+      return false;
+    }else{
+      //enviarDatos();
+      return true;
+    }
 }
 
-// ------------------------------
 
-function leerDatos(valor,sel,opcion) {
-  crearObjeto();
-  if (objeto.readyState != 0) {
-    alert('Error al crear el objeto XML. El Navegador no soporta AJAX');
-  } else {
-    // Preparar donde va a recibir el Resultado
-    objeto.onreadystatechange = procesaResultado;
-var ca=/^[ ]{1}/;
-var com=ca.test(valor);
-	if((!com=="") || (valor=="")){document.getElementById("resultado").innerHTML="";}else{
-	// Enviar la consulta
-	    objeto.open("GET", "paginas/busqueda.php?opc="+sel+"&usuario=" + valor + "&inscrito="+opcion, true);
-	    objeto.send(null);
-	}
-    
-  }
+
+
+  $(function() {
+    $('#txtFechaRegistro').datepicker({
+      dateFormat: 'yy-mm-dd', 
+      changeMonth: true, 
+      changeYear: true, 
+      yearRange: '+0:+10',
+	  minDate: -0, maxDate: "+0D"
+      });
+    });
+
+    $(function() {
+  //Se pone para que en todos los llamados ajax se bloquee la pantalla mostrando el mensaje Procesando...
+  $.blockUI.defaults.message = 'Procesando información, por favor espere... <br /><img src=\'img/load.gif\' /><br />';
+  $(document).ajaxStart($.blockUI).ajaxStop($.unblockUI);
+});
+function enviarDatos(){
+  var formulario = $("#hongkiat-form").serializeArray();
+    $.ajax({
+      type: "POST",
+      dataType: 'json',
+        url: "procesos/guardarOferta.php",
+        data: formulario,
+    }).done(function(respuesta){
+        if(respuesta.mensaje==1){
+          alert("Registro realizado con exito");
+		  document.getElementById('hongkiat-form').reset();
+          document.getElementById('txtNombre').focus();
+        }else if(respuesta.mensaje==2){
+          alert("No fue posible registrar la Oferta");
+        }
+    });
 }
-function leerDatosPag(valor,sel,pag,opcion) {
-  crearObjeto();
-  if (objeto.readyState != 0) {
-    alert('Error al crear el objeto XML. El Navegador no soporta AJAX');
-  } else {
-    // Preparar donde va a recibir el Resultado
-    objeto.onreadystatechange = procesaResultado;
-var ca=/^[ ]{1}/;
-var com=ca.test(valor);
-	if((!com=="") || (valor=="")){document.getElementById("resultado").innerHTML="";}else{
-	// Enviar la consulta
-	    objeto.open("GET", "paginas/busqueda.php?opc="+sel+"&usuario=" + valor + "&pagina="+pag+ "&inscrito="+opcion, true);
-	    objeto.send(null);
-	}
-    
-  }
-}
-// ------------------------------
+$(document).ready(function(){         
+  $("#submitbtn").click(function(){
+    if(validar()){
+        enviarDatos();
+      }
+      return false;
+  });
+});
 </script>
-<script type='text/javascript' language='Javascript'>
-function seleccionar(){
-	/*document.getElementById("cargarCaja").style="display:none;";
-	document.getElementById("fecha").style.diplay="none";*/
-	var tipo=document.hongkiat.lstTipo.options[document.hongkiat.lstTipo.selectedIndex].value;
-	//document.getElementById("txtFecha").style="display:none;";
-	document.getElementById("dui").style.display="none";
-	if(tipo==1){
-		document.getElementById("cargarCaja").style.display="none";
-		document.getElementById("dui").style.display="";
-	}else if(tipo==2){
-		document.getElementById("cargarCaja").innerHTML="<input type='text' name='txtTarjeta' onKeyPress='leerDatos(document.hongkiat.txtTarjeta.value,document.hongkiat.lstTipo.value,document.hongkiat.inscrito.value);' class='id txtinput' id='txtTarjeta' placeholder='Digite el numero de Tarjeta' autocomplete='off' tabindex='1' style='width:25%;'>";
-	}else if(tipo==3){
-		document.getElementById("cargarCaja").innerHTML="<input type='text' name='txtNombre' onKeyPress='leerDatos(document.hongkiat.txtNombre.value,document.hongkiat.lstTipo.value,document.hongkiat.inscrito.value);' class='id txtinput' id='txtNombre' placeholder='Digite el nombre a buscar' autocomplete='off' tabindex='1' style='width:25%;'>";
-	}else{
-		document.getElementById("cargarCaja").innerHTML="";
-	}
-}
+<script src="js/mask.js"></script>
+<script>
+  jQuery(function($){
+     $("#txtDescuento").mask("0.99");   
+  });
 </script>
-<br />
-<br />
 <form name="hongkiat" id="hongkiat-form">
 <div id="wrapping" class="clearfix">
   <section id="aligned" style='text-align:center;'>
   <h2> HOJA DE PEDIDOS</h2>
-    <h3>Nombre de supervisora:</h3>
-    <div id="dui" style='display:;clear:both;'> 
-  		<input type="text" name="txtSupervisora" class="search txtinput" id="txtSupervisora" placeholder="nombre, apellido, usuario" autocomplete="off" tabindex="1" style="width:25%;" value="SUPERVISORA ANDREA">
-  	</div>
-    <h3>Fecha de pedido:</h3>
-    <input type="text" name="txtFecha" class="datepicker calendar txtinput" id="txtFecha" placeholder="Fecha a buscar" autocomplete="off" tabindex="1">
-    <h3>Sucursal:</h3>
-    <div id="dui" style='display:;clear:both;'> 
-      <input type="text" name="txtSucursal" class="search txtinput" id="txtSucursal" placeholder="nombre, apellido, usuario" autocomplete="off" tabindex="1" style="width:25%;" value="LA JOYA">
-    </div>
+  <h3>Empleado:</h3>
+  <?php 
+        $objeto=new CasaMunoz;
+      $consultarsucu=$objeto->consultar_empleado_sucursales($_SESSION['sucursal']);
+	  
+	       echo "<select name='lstEmpleadoSucursal' id='lstEmpleadoSucursal' class='selmenu'>";
+      echo "<option value='0'>Elige</option>";
 
-    <div id="resultado" style='background:black;'>
-    	<table width='100%' style="font-size:12pt;">
-    		<tr style="background:white;">
-    			<th>Cantidad</th>
-    			<th>Descripcion</th>
-    			<th>Si</th>
-    			<th>No</th>
-    		</tr>
-    		<tr style="background:silver;">
-          <td>Litro</td>
-    			<td>Ablandador</td>
-    			<td><input type="radio" name="rdSi1"  checked="checked" /></td>
-    			<td><input type="radio" name="rdSi1" /></td>
-    		</tr>
-    		<tr style="background:white;">
-          <td>4 onzas</td>
-          <td>Acetona</td>
-          <td><input type="radio" name="rdSi2" /></td>
-          <td><input type="radio" name="rdSi2" checked="checked" /></td>
-    		</tr>
-    		<tr style="background:silver;">
-    			<td>4 onzas</td>
-          <td>Agua oxigenada</td>
-          <td><input type="radio" name="rdSi3" checked="checked" /></td>
-          <td><input type="radio" name="rdSi3" /></td>
-    		</tr>
-    	</table>
-    </div>
+      while($sucu=$consultarsucu->fetch(PDO::FETCH_OBJ))
+      {
+        echo "<option value='".$sucu->cod_emp."'>".$sucu->NombreCompleto."</option>";
+      }
+      echo "</select>";
+   ?> 
+  
+    <h3>Fecha de pedido:</h3>
+    <input type="text" name="txtFechaRegistro" id="txtFechaRegistro" placeholder="Fecha de registro de oferta" autocomplete="off" tabindex="1" class="txtinput calendar" readonly="readonly">
+    <h3>Sucursal:</h3>
+     <?php 
+        $objeto=new CasaMunoz;
+      $consultarsucu=$objeto->consultar_sucursal_unica($_SESSION['sucursal']);
+	  
+      while($sucu=$consultarsucu->fetch(PDO::FETCH_OBJ))
+      {
+        echo "<option value='".$sucu->cod_sucursal."'>".$sucu->nom_sucursal."</option>";
+      }
+      echo "</select>";
+   ?> 
+  </br>
+    <label>PEDIDOS</label>
+      <table width="100%" style="font-size:10pt;border-color:black;" border="1">
+        <tr>
+          <th>Cantidad</th>
+          <th>Producto</th>
+          <th>Seleccionar</th>
+        </tr>
+
+<?php 
+
+ $consultarproducto=$objeto->consultar_producto();
+        while($producto=$consultarproducto->fetch(PDO::FETCH_OBJ))
+        {
+
+		
+		
+		echo "<tr>
+		  <td><input type='number' name='txtCantPedido[]' id='txtCantPedido[]' min=0 /></td>
+            <td>".$producto->nom_producto."</td>
+			<td><input type='checkbox' name='chkproducto[]' id='chkproducto[]' /></td>
+            
+          </tr>";
+        }
+        ?>
+      </table>
+	 
+			
+
   </section>
 </div>
+<section id="buttons">
+        <input type="submit" name="submit" id="submitbtn" class="submitbtn" tabindex="7" value="Guardar">
+        <br style="clear:both;">
+    </section>
 </form>
