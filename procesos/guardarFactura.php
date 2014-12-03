90236e144218728c5ee6860e467ae17d5e217785
amtliz <lizrfp@gmail.com>
2014-12-03 12:25:08 -0600

    Guarda Factura, falta actualizar estado de reserva

---
+++ procesos/guardarFactura.php
@@ -0,0 +1,17 @@
+<?php
+include("header.php");
+sleep(1);
+session_start();
+$respuesta = new stdClass();
+extract($_POST);
+//print_r($_POST);
+$factura=new CasaMunoz;
+$ResFactura=array($txtNoFact, $rdSeleccionar, $_SESSION['sucursal']);
+//print_r($Resfactura);
+if($factura->guardar_factura($ResFactura)){
+        $respuesta->mensaje = 1;
+}else{
+        $respuesta->mensaje = 2;
+}
+echo json_encode($respuesta);
+?>
